ae65735294cbc512d9fba8ed3d79f1d4b9f5c4d1
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-06-03 12:10:29 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - prints all single digit numbers of base 10 starting from 0
+ * main - prints all single digit numbers of base 10 starting from 0
  * Return: 0 if successful
  */
 int main(void)
