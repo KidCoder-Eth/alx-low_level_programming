5153ea60343f513cf86756fa751a014fc2c93e30
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-06-07 19:28:40 -0700

    Task-3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islower – checks for lowercase character
+ * Description: _islower - checks for lowercase character
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
