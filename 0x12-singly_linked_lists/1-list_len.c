db5fedea80de035ad336ddeac1e27a24502239ef
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-07-14 11:01:00 -0700

    Task-1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,11 +6,12 @@
  */
 size_t list_len(const list_t *h)
 {
-	int i;
+	size_t n = 0;
 
-	if (h == NULL)
-		return (0);
-	for (i = 1; h->next != NULL; i++)
+	while (h)
+	{
+		n++;
 		h = h->next;
-	return (i);
+	}
+	return (n);
 }
