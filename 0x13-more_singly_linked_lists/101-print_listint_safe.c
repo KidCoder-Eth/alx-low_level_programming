5b3f5e1693fa509d52636b2ceb2674a82499d72e
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-07-18 09:21:26 -0700

    Task-12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,8 @@
 #include "lists.h"
+
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * print_listint_safe - prints a linked list safely
+ * @head: list of type listint to print
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
